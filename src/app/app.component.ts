7e6711227d38861ab24addd356dc06532c2f0330
maros <marcos.ortega94@gmail.com>
2020-07-24 15:22:24 -0400

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-pwa-app';
+export class AppComponent implements OnInit {
+    title = 'angular-pwa-app';
+
+    constructor(private swUpdate: SwUpdate) {
+    }
+
+    ngOnInit(): void {
+        // check back to the server to see i new ngsw.json is avaible
+        //  this can be calles inside setInterval, upon router navigation, (and something else)
+        this.swUpdate.checkForUpdate();
+        this.swUpdate.available.subscribe(()=>{
+            if(confirm("New version avaible. Load New Version?")){
+                window.location.reload();
+            }
+        });
+
+    }
 }
